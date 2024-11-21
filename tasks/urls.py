cec33014335c954eb07d2de12a89bcf2cd5af327
Nhan Bui <buithiennhan0345@gmail.com>
2024-11-21 16:16:33 +0700

    Config route for task apis
    Fix middleware
    
    Fix auth middleware

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('<int:id>', get_task_by_id),  # POST
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from utils.response import success_response, failure_response
+from middlewares import auth_middleware
+from rest_framework import status
 
-# Create your views here.
+
+@swagger_auto_schema(
+    method='GET',
+    operation_description="Get task by id",
+    tags=["Task"],
+    responses={
+        200: openapi.Response(
+            "Success",
+        ),
+        400: openapi.Response(
+            "Validation Error", examples={"application/json": {"error": "Invalid input"}}
+        )
+    },
+    security=[]
+)
+@api_view(['GET'])
+@auth_middleware
+def get_task_by_id(request, id, *args, **kwargs):
+    print(id)
+    return success_response(status_code=status.HTTP_200_OK)
