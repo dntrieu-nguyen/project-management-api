9487648cc8db53daefee54085411c0ab3e63c4e8
Nhan Bui <100027649+nhanbui1512@users.noreply.github.com>
2024-11-22 15:12:00 +0700

    Feat/auth (#7)
    * Handle auth_middleware

--- auths/urls.py
+++ auths/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('login/', login),  # POST
-    path('register/', register),  # POST
-    path('me/', profile_view, name='update_profile'),  # PATCH
-    path('password/', change_password),  # PATCH
+    path('login', login),  # POST
+    path('register', register),  # POST
+    path('me', profile_view, name='get_update_profile'),  # PATCH / GET
+    path('password', change_password),  # PATCH
+    path('refresh_token', refresh_token)
 ]
