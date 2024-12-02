ea5eac665328327c5b765b592911b46cf0591920
dntrieu-nguyen <nguyen.dang@rikai.technology>
2024-12-02 15:49:14 +0700

    refactor: update create, seen notificaiton

--- notifications/views.py
+++ notifications/views.py
@@ -1,28 +1,25 @@
 import datetime
+from uuid import UUID
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
 from app.models import User
 from middlewares import auth_middleware
-from notifications.serializers import NotificationRequestUpdateStatusSerializers, NotificationsRequestByUserIdSerializers, NotificationsRequestCreateSerializers
+from notifications.serializers import  NotificationsRequestCreateSerializers
 from utils.response import failure_response, success_response
 from firebase.firebase_config import db
+from rest_framework import status
 
 @api_view(['GET'])
+@auth_middleware
 def get_all_notifications_by_user(request):
     """
     Lấy tất cả thông báo của người dùng dựa trên ID.
     """
-    # Sử dụng query parameters thay vì request.data cho GET request
-    user_id = request.data['id']
-    
-    if not user_id:
-        return failure_response(
-            message='Validation Errors',
-            data={'id': ['This field is required.']}
-        )
+    user_id = request.user['id']
 
     ref = db.reference(f'notifications/{user_id}')
+
     snapshot = ref.get()
 
     if not snapshot:
@@ -37,9 +34,6 @@ def get_all_notifications_by_user(request):
 @api_view(['POST'])
 @auth_middleware
 def send_notifications_to_user(request):
-    """
-    Gửi thông báo đến một người dùng.
-    """
     req_body = NotificationsRequestCreateSerializers(data=request.data)
     sender = request.user
 
@@ -49,58 +43,57 @@ def send_notifications_to_user(request):
             data=req_body.errors
         )
     
-     # check existed sender
-    is_existed_sender = User.objects.filter(id=sender['id'])
-
-    if not is_existed_sender:
+    # Check existed sender
+    if not User.objects.filter(id=sender['id']).exists():
         return failure_response(message="sender not found")
     
-    receiver = User.objects.filter(id=req_body.validated_data['user_id'])
-
+    receiver = User.objects.filter(id=req_body.validated_data['user_id']).first()
     if not receiver:
         return failure_response(message="user not found")
     
-    req_body.validated_data['sender_id'] = sender['id'].replace('-','')
-    req_body.validated_data['created_at'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    user_id = req_body.validated_data['user_id']
-    ref = db.reference(f'notifications/{user_id}')
-    snapshot = ref.push(req_body.validated_data) 
+    notification_data = req_body.validated_data
+    notification_data['sender_id'] = sender['id']
+    notification_data['created_at'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    notification_data['updated_at'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    
+    user_id = notification_data['user_id']
+    ref = db.reference(f'notifications/{UUID(user_id)}')
+    
+    # Uncomment the following line if you need to save the notification
+    snapshot = ref.push(notification_data) 
 
     return success_response(
         message="Notification sent successfully",
-        data={"notification_id": snapshot.key} 
+        # Uncomment this line if you need to return the notification ID
+        data={"notification_id": snapshot.key}
     )
 
-@api_view(['PUT'])
+@api_view(['PATCH'])
 @auth_middleware
-def update_status_notifications(request):
-    req_body = NotificationRequestUpdateStatusSerializers(data=request.data)
-    user_id = request.user['id']
+def seen_notification_by_user(request):
+    try:
+        user_id = request.user['id']
 
+        notification_id = request.query_params['notification_id']
 
-    if not req_body.is_valid():
+        if not bool(notification_id):
+            return failure_response(
+                message="Validation errors",
+                data= {
+                    "notification_id": "required"
+                }
+            )
+
+        ref = db.reference(f"notifications/{user_id}/{notification_id}")
+
+        ref.update({
+            "is_read":True,
+            "updated_at": datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        })
+
+        return success_response(message="update successfully")
+    except Exception as e:
         return failure_response(
-            message="Validation Errors",
-            data=req_body.errors
+           message=str(e),
+           status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
-
- 
-    notification_ref = db.reference(f'notifications/{str(user_id).replace('-', '')}/{req_body.validated_data["notification_id"]}')
-    current_data = notification_ref.get() 
-
-    if not current_data:
-        return failure_response(message="Notification not found")
-
-   
-    update_data = {
-        'is_read': True,
-        'updated_at': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    }
-
- 
-    notification_ref.update(update_data)
-
- 
-    detail_notification_snapshot = notification_ref.get()
-
-    return success_response(message='Notification updated successfully', data=detail_notification_snapshot)
